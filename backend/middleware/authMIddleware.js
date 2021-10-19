bbf755ff12f518a0733ca655aa996bce7f6f1deb
LakeCombs <adesolaolamilekan2@gmail.com>
2021-10-19 06:10:53 +0100

    auth middleware

---
+++ backend/middleware/authMIddleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const asyncHandler = require("express-async-handler");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      //decode the token id
+      const decodeSecret = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decodeSecret.id).select("-password");
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized, no token");
+    }
+  }
+});
+
+module.exports = protect;

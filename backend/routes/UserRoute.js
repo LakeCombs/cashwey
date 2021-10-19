cdf0910ab4fa9f48666229b439d7255cf18d2ba5
LakeCombs <adesolaolamilekan2@gmail.com>
2021-10-19 06:02:09 +0100

    user router

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const {
+  siginInUser,
+  registerUser,
+  editUser,
+} = require("../controller/UserController");
 
 const router = express.Router();
+router.route("/").post(registerUser);
+router.route("/login").post(siginInUser);
+router.route("/profile").post(editUser);
 
 module.exports = router;
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,4 +4,4 @@ const generateToken = (id) => {
   return JWT.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 
-export default generateToken;
+module.exports = generateToken;
